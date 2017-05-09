047118909ff0c2800901d3115773a56b978e6819
alex shyaka <shyakaster@gmail.com>
2017-05-09 14:21:44 +0300

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -19,7 +19,8 @@ gem 'jbuilder',                '2.4.1'
 
 group :development, :test do
   gem 'sqlite3', '1.3.12'
-  gem 'byebug',  '9.0.0', platform: :mri
+  # gem 'byebug',  '9.0.0'
+  gem 'pry', '~> 0.10.4'
 end
 
 group :development do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def create
+  	@user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to the sample app"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
